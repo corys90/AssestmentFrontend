7d97726885786f145cd8f190c0d28d8205055061
CristianCortes <corys90@hotmail.com>
2022-04-22 13:52:42 -0500

    aupdate homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useState } from "react";
 import { React } from "react";
 import CardHome from "../../components/CardHome";
@@ -6,11 +6,13 @@ import "../HomePage/style.css";
 
 const HomePage = () => {
   const [data, setData] = useState([]);
+  const waitting = useRef(null);
 
   async function fakerApi() {
     await fetch("https://fakestoreapi.com/products/")
       .then((res) => res.json())
       .then((json) => {
+        waitting.current.removeChild(waitting.current.children[0]);
         setData(json);
         return json;
       });
@@ -22,6 +24,7 @@ const HomePage = () => {
 
   return (
     <div className="centerDivHP">
+      <div ref={waitting}>loading, wait!!!</div>
       {data.map((element, idx) => (
         <div key={idx} className="centerDivHP--productItems">
           <CardHome
